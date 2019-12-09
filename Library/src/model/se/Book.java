c170ce1da16131100f5e570f18d24c8a10a0a591
AFF77 <56636954+AFF77@users.noreply.github.com>
2019-12-09 02:38:39 +0100

    Book.java
    gett/setter klart

--- Library/src/model/se/Book.java
+++ Library/src/model/se/Book.java
@@ -10,5 +10,33 @@ package model.se;
  * @author aysen
  */
 public class Book {
+    private String title, author;
+    private int count;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+    
+    
     
 }
